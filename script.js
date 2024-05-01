a9ddd1b27ecb381eca41ce964846521f2c0e225c
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-01 16:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstChar(text) {
   // your code here
+	let newtext=text.trim();
+	if(newtext.length==0)return ""
+	else return newtext[0];
 }
-
-// Do not change the code below
+  
+// Do not change the code below 
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
